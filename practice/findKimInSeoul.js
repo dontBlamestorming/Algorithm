7e9a033af9a568925faaf6e5b483b1b2bcd70acc
dave <lklone2005@gmail.com>
2020-08-25 22:00:30 +0900

    서울에서 김서방 찾기

---
+++ practice/findKimInSeoul.js
@@ -0,0 +1,26 @@
+/*
+    문자열 내림차순으로 배치하기
+    url: https://programmers.co.kr/learn/courses/30/lessons/12916#
+*/
+
+function solution(seoul) {
+  let answer = "";
+  let indexOfKim = 0;
+
+  for (let i = 0; i < seoul.length; i++) {
+    if (seoul[i] === "Kim") {
+      indexOfKim = i;
+    }
+  }
+
+  return (answer = `김서방은 ${indexOfKim}에 있다`);
+}
+
+/*
+    내장 함수 이용하기
+
+    function solution(seoul) {
+        const index = seoul.indexOf("Kim");
+        return `김서방은 ${index}에 있다`;
+    }
+*/
